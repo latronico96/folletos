6626f462725d2903ad7c86a15ee6e201877e302c
latronico96 <35084047+latronico96@users.noreply.github.com>
2025-06-12 23:35:01 -0300

    usar HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter , Routes, Route } from 'react-router-dom';
 import { Container } from '@mui/material';
 import FolletoCocol from './pages/FolletoCocol';
 import About from './pages/About';
@@ -6,14 +6,14 @@ import './App.css'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter >
       <Container sx={{ mt: 4 }}>
         <Routes>
           <Route path="/" element={<About />} />
           <Route path="/Cocol" element={<FolletoCocol />} />
         </Routes>
       </Container>
-    </BrowserRouter>
+    </HashRouter >
   );
 }
 
