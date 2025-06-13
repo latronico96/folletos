c84bd08ea09aac99903a55ae9a4718387f15ce42
latronico96 <35084047+latronico96@users.noreply.github.com>
2025-06-12 23:25:57 -0300

    sacar el as y borrar imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { AppBar, Toolbar, Button, Container } from '@mui/material';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Container } from '@mui/material';
 import FolletoCocol from './pages/FolletoCocol';
 import About from './pages/About';
 import './App.css'
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Container sx={{ mt: 4 }}>
         <Routes>
           <Route path="/" element={<About />} />
           <Route path="/Cocol" element={<FolletoCocol />} />
         </Routes>
       </Container>
-    </Router>
+    </BrowserRouter>
   );
 }
 
